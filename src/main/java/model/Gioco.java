06c3390750da8db0df2a20b69e8eebe50aeb9d3b
Antonio Orefice <antonio.orefice8@studenti.unina.it>
2026-05-09 00:18:53 +0200

    Aggiunti metodi per relazioni, getter e setter.

--- src/main/java/model/Gioco.java
+++ src/main/java/model/Gioco.java
@@ -29,4 +29,46 @@ public class Gioco {
         generi.add(genere);
         piattaforme.add(piattaforma);
     }
+
+    public void addPromozione(GiocoInPromozione promozione){
+        if (promozione == null){
+            throw new IllegalArgumentException("Promozione non esistente");
+        }
+        promozioni.add(promozione);
+    }
+
+    public void addFattura(Fattura fattura){
+        if (fattura == null){
+            throw new IllegalArgumentException("Fattura non esistente");
+        }
+        fatture.add(fattura);
+    }
+
+    public void addGenere(Genere genere){
+        if (genere == null){
+            throw new IllegalArgumentException("Genere non disponibile");
+        }
+        generi.add(genere);
+    }
+
+    public void addPiattaformaDiGioco(PiattaformaDiGioco piattaforma){
+        if (piattaforma == null){
+            throw new IllegalArgumentException("Piattaforma di gioco non disponibile");
+        }
+        piattaforme.add(piattaforma);
+    }
+
+    //Lista di get
+    public String getTitolo() {return titolo;}
+    public double getPrezzo() {return prezzo;}
+    public Date getDataRilascio() {return dataRilascio;}
+    public String getCategoria() {return categoria;}
+    public int getPegi() {return pegi;}
+    public Sviluppatore getSviluppatore() {return sviluppatore;}
+    public ArrayList<Genere> getGeneri() {return generi;}
+    public ArrayList<PiattaformaDiGioco> getPiattaformeDiGioco() {return piattaforme;}
+
+    //set
+    public void setPrezzo(double prezzo) {this.prezzo = prezzo;}
+
 }
