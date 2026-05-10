0e1ddb992494703d4db152f2d2b733c27b7863af
Antonio Orefice <antonio.orefice8@studenti.unina.it>
2026-05-10 21:09:15 +0200

    Rimossi prezzo, data e relazione con PiattaformaDiGioco data la futura creazione della classe associativa EdizioneGioco per implementare le edizioni dello stesso videogioco come oggetti diversi.

--- src/main/java/model/Gioco.java
+++ src/main/java/model/Gioco.java
@@ -1,33 +1,28 @@
 package model;
 
 import java.util.ArrayList;
-import java.util.Date;
 
 public class Gioco {
     private String titolo;
-    private double prezzo;
-    private Date dataRilascio;
     private String categoria;
     private int pegi;
 
     // Relazioni
     private Sviluppatore sviluppatore;
     private ArrayList<Genere> generi = new ArrayList<>();
-    private ArrayList<PiattaformaDiGioco> piattaforme = new ArrayList<>();
+    private ArrayList<EdizioneGioco> edizioni = new ArrayList<>();
     private ArrayList<GiocoInPromozione> promozioni = new ArrayList<>();
     private ArrayList<Fattura> fatture = new ArrayList<>();
 
-    public Gioco(String titolo, double prezzo, Date dataRilascio, String categoria, int pegi, Sviluppatore sviluppatore, Genere genere, PiattaformaDiGioco piattaforma) {
+    public Gioco(String titolo, String categoria, int pegi, Sviluppatore sviluppatore, Genere genere, EdizioneGioco edizione) {
 
         this.titolo = titolo;
-        this.prezzo = prezzo;
-        this.dataRilascio = dataRilascio;
         this.categoria = categoria;
         this.pegi = pegi;
 
         this.sviluppatore = sviluppatore;
         generi.add(genere);
-        piattaforme.add(piattaforma);
+        edizioni.add(edizione);
     }
 
     public void addPromozione(GiocoInPromozione promozione){
@@ -51,24 +46,20 @@ public class Gioco {
         generi.add(genere);
     }
 
-    public void addPiattaformaDiGioco(PiattaformaDiGioco piattaforma){
-        if (piattaforma == null){
-            throw new IllegalArgumentException("Piattaforma di gioco non disponibile");
+    public void addEdizione(EdizioneGioco edizione){
+        if (edizione == null){
+            throw new IllegalArgumentException("Edizione di gioco non disponibile");
         }
-        piattaforme.add(piattaforma);
+        edizioni.add(edizione);
     }
 
     //Lista di get
     public String getTitolo() {return titolo;}
-    public double getPrezzo() {return prezzo;}
-    public Date getDataRilascio() {return dataRilascio;}
     public String getCategoria() {return categoria;}
     public int getPegi() {return pegi;}
     public Sviluppatore getSviluppatore() {return sviluppatore;}
     public ArrayList<Genere> getGeneri() {return generi;}
-    public ArrayList<PiattaformaDiGioco> getPiattaformeDiGioco() {return piattaforme;}
+    public ArrayList<EdizioneGioco> getEdizioni() {return edizioni;}
 
-    //set
-    public void setPrezzo(double prezzo) {this.prezzo = prezzo;}
 
 }
