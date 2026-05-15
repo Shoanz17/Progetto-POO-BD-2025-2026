4e920e7a9605a424766785114d9c550cd2872d6d
Ciro Vivenzio <cirovivenzio58@gmail.com>
2026-05-15 14:19:45 +0200

    Cambiati gli attributi da protected a private, cambiato il tipo dell' attributo "dataCreazione" da Date a Local Date, aggiunto il costruttore per il DAO, aggiunto nuovamente il "getPassword" e aggiunto nel metodo "setPassword" un vincolo.

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -4,46 +4,57 @@ package model;
 import org.postgresql.shaded.com.ongres.stringprep.Stringprep;
 
 
-import java.util.Date;
+import java.time.LocalDate;
+
 
 public class Account {
     // attributi in account:
-    protected String nome;
-    protected int id;
-    protected String password;
-    protected Date dataCreazione;
+        private String nome;
+        private int id;
+        private String password;
+        private LocalDate dataCreazione;
 
     //Creazione del costruttore
-    public Account(String nome,int id,String password,Date dataCreazione)
+    public Account(String nome,int id,String password)
     {
         this.nome = nome;
         this.id = id;
         this.password = password;
-        this.dataCreazione = new Date();//costruisce l'oggetto data,che da la data nell' istante in cui viene creata
+        this.dataCreazione = LocalDate.now();
+        //localdate mette esattamente la data in cui viene creato l'oggetto
 
     }
-    //get e set
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
+    //costruttore per il DAO
+    public Account(String nome,String password)
+    {
         this.nome = nome;
+        this.password = password;
+        this.dataCreazione = LocalDate.now();
+
     }
 
-    public int getId() {
-        return id;
-    }
+    //get e set
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}//nome deve essere grande quanto il var char in db
 
+    public int getId() {return id;}
+
+    public String getPassword() {
+        return password;
+    }
     public void setPassword(String password) {
+
+        if (password.length()<8) {
+            throw new IllegalArgumentException("La password richiede almeno 8 caratteri.");
+        }
         this.password = password;
     }
 
-    public Date getDataCreazione() {
+    public LocalDate getDataCreazione() {
         return dataCreazione;
     }
 
 }
 
 
-//mancano metodi per cambiare sia il nome che la password
+
