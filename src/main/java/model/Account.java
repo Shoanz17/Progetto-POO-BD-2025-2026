41694960c32ae5896394bc08c1a416033682f465
Ciro Vivenzio <cirovivenzio58@gmail.com>
2026-05-16 18:42:42 +0200

    Aggiunto altri vincoli per il setPassword, aggiunto il vincolo per il setNome,sistemato il costruttore e il costruttore per il DAO.

--- src/main/java/model/Account.java
+++ src/main/java/model/Account.java
@@ -15,27 +15,31 @@ public class Account {
         private LocalDate dataCreazione;
 
     //Creazione del costruttore
-    public Account(String nome,int id,String password)
+    public Account(String nome,String password)
     {
-        this.nome = nome;
-        this.id = id;
-        this.password = password;
+        setNome(nome);
+        setPassword(password);
         this.dataCreazione = LocalDate.now();
-        //localdate mette esattamente la data in cui viene creato l'oggetto
 
     }
     //costruttore per il DAO
-    public Account(String nome,String password)
+    public Account(String nome,int id,String password,LocalDate dataCreazione)
     {
         this.nome = nome;
         this.password = password;
-        this.dataCreazione = LocalDate.now();
-
+        this.id = id;
+        this.dataCreazione = dataCreazione;
+        //localdate mette esattamente la data in cui viene creato l'oggetto
     }
 
     //get e set
     public String getNome() {return nome;}
-    public void setNome(String nome) {this.nome = nome;}//nome deve essere grande quanto il var char in db
+    public void setNome(String nome)
+    {
+        if(nome.length()>24)
+        { throw new IllegalArgumentException("il nome che hai scelto \""+ nome +"\" è troppo lungo");}
+        this.nome = nome;
+    }//nome deve essere grande quanto il var char in db
 
     public int getId() {return id;}
 
@@ -43,18 +47,33 @@ public class Account {
         return password;
     }
     public void setPassword(String password) {
-
+        boolean haMaiuscola = false;
+        boolean haNumero = false;
+        boolean haSpeciale = false;
+        // qui controlliamo se la password contiene almeno 8 caratteri
         if (password.length()<8) {
             throw new IllegalArgumentException("La password richiede almeno 8 caratteri.");
         }
+        //usiamo il ciclo for per controllare ogni carattere, la password deve avere almeno una maiuscola
+        // un numero e un carattere speciale
+        for(int i = 0; i < password.length(); i++)
+        {
+            char lettera = password.charAt(i);
+            if(Character.isUpperCase(lettera))
+            {haMaiuscola = true;}
+            else if (Character.isDigit(lettera))
+            {haNumero = true;}
+            else if (!Character.isLetterOrDigit(lettera)&& !Character.isWhitespace(lettera))
+            {haSpeciale = true;}
+        }
+        // qui nel ciclo if se non ho maiuscola o numero o speciale lanciamo la throw
+        if(!haMaiuscola||!haNumero||!haSpeciale)
+        {throw new IllegalArgumentException
+                ("La password deve contenere almeno una maiuscola," +  " un numero e un carattere speciale.");}
+
         this.password = password;
     }
 
-    public LocalDate getDataCreazione() {
-        return dataCreazione;
-    }
+    public LocalDate getDataCreazione() {return dataCreazione;}
 
 }
-
-
-
