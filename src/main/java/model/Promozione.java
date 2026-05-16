b29cc691dee8318cd9f0b76b1aef0685e0def68d
Antonio Orefice <antonio.orefice8@studenti.unina.it>
2026-05-16 17:08:01 +0200

    Aggiunta relazione con "GiocoInPromozione" con relativo metodo di aggiunta.

--- src/main/java/model/Promozione.java
+++ src/main/java/model/Promozione.java
@@ -1,16 +1,27 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Promozione {
     private String nome;
     private LocalDate dataInizio;
     private LocalDate dataFine;
 
+    //relazione
+    private ArrayList<GiocoInPromozione> giochiInPromozione = new ArrayList<>();
+
     public Promozione(String nome, LocalDate dataInizio, LocalDate dataFine) {
         this.nome = nome;
         this.dataInizio = dataInizio;
         this.dataFine = dataFine;
+    }
+
+    public void addGiocoInPromozione(GiocoInPromozione giocoInPromozione){
+        if (giocoInPromozione == null){
+            throw new IllegalArgumentException("Il gioco in promozione non esiste");
+        }
+        giochiInPromozione.add(giocoInPromozione);
     }
 
 }
