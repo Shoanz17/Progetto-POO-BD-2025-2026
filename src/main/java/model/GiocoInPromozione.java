6d4781d44c1695294033bfc5c668af5705f6cc2b
Antonio Orefice <antonio.orefice8@studenti.unina.it>
2026-05-16 18:22:25 +0200

    Creati metodi setter e getter con relativi vincoli e sicurezze

--- src/main/java/model/GiocoInPromozione.java
+++ src/main/java/model/GiocoInPromozione.java
@@ -10,8 +10,27 @@ public class GiocoInPromozione {
     public GiocoInPromozione(int percentuale,Gioco gioco, Promozione promozione){
         setPercentuale(percentuale);
 
-        setGioco(gioco);
-        setPromozione(promozione);
+        if(gioco == null){
+            throw new IllegalArgumentException("Gioco non esistente");
+        }
+        this.gioco = gioco;
+
+        if(promozione == null){
+            throw new IllegalArgumentException("Promozione non esistente");
+        }
+        this.promozione = promozione;
     }
 
+    //setter
+    public void setPercentuale(int percentuale) {
+        if(percentuale < 0 || percentuale > 100){
+            throw new IllegalArgumentException("Lo sconto non puó essere minore di 0 o maggiore di 100");
+        }
+        this.percentuale = percentuale;
+    }
+
+    //getter
+    public int getPercentuale() {return percentuale;}
+    public Promozione getPromozione() {return promozione;}
+    public Gioco getGioco() {return gioco;}
 }
