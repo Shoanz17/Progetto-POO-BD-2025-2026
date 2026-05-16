919d6a65d436e10c438e814784b5fc2f8be11b03
Antonio Orefice <antonio.orefice8@studenti.unina.it>
2026-05-16 17:14:09 +0200

    Aggiunti metodi getter e setter dei relativi attributi. Aggiunti metodi nel costruttore per aggiunta di vincoli semantici e controlli di sicurezza.

--- src/main/java/model/Promozione.java
+++ src/main/java/model/Promozione.java
@@ -12,16 +12,46 @@ public class Promozione {
     private ArrayList<GiocoInPromozione> giochiInPromozione = new ArrayList<>();
 
     public Promozione(String nome, LocalDate dataInizio, LocalDate dataFine) {
-        this.nome = nome;
-        this.dataInizio = dataInizio;
-        this.dataFine = dataFine;
+        setNome(nome);
+        setDataInizio(dataInizio);
+        setDataFine(dataFine);
     }
 
     public void addGiocoInPromozione(GiocoInPromozione giocoInPromozione){
         if (giocoInPromozione == null){
-            throw new IllegalArgumentException("Il gioco in promozione non esiste");
+            throw new IllegalArgumentException("Gioco in Promozione non esistente");
         }
         giochiInPromozione.add(giocoInPromozione);
     }
 
+    // Lista get
+    public String getNome() {return nome;}
+    public LocalDate getDataInizio() {return dataInizio;}
+    public LocalDate getDataFine() {return dataFine;}
+    public ArrayList<GiocoInPromozione> getGiochiInPromozione() {return giochiInPromozione;}
+
+    // Lista set
+    public void setNome(String nome) {
+        if (nome == null || nome.trim().isEmpty() || nome.length() > 30) {
+            throw new IllegalArgumentException("Nome immesso della promozione non valido");
+        }
+        this.nome = nome;
+    }
+
+    public void setDataInizio(LocalDate dataInizio) {
+        if (dataInizio == null) {
+            throw new IllegalArgumentException("Immettere una data di Inizio valida");
+        }
+        this.dataInizio = dataInizio;
+    }
+
+    public void setDataFine(LocalDate dataFine) {
+        if (dataFine == null) {
+            throw new IllegalArgumentException("Immettere una data di fine valida");
+        }
+        if (this.dataInizio != null && dataFine.isBefore(this.dataInizio)) {
+            throw new IllegalArgumentException("La data di fine DEVE essere DOPO la data di inizio");
+        }
+        this.dataFine = dataFine;
+    }
 }
