64e99634d3e9f8113125f90df8fc27fda68684b3
Antonio Orefice <antonio.orefice8@studenti.unina.it>
2026-05-17 16:15:50 +0200

    Creati metodi getter e setter con relativi vincoli semantici e sicurezze.

--- src/main/java/model/Recensione.java
+++ src/main/java/model/Recensione.java
@@ -19,5 +19,26 @@ public class Recensione {
         this.fattura = fattura;
     }
 
+    //setter
+    public void setVoto (int voto){
+        if(voto < 0 || voto > 100){
+            throw new IllegalArgumentException("Immettere un voto tra 0 e 100.");
+        }
+        this.voto = voto;
+    }
 
+    public void setDescrizione (String descrizione){
+        if(descrizione == null || descrizione.trim().isEmpty() || descrizione.length() > 500 ){
+            throw new IllegalArgumentException("La descrizione deve essere di un massimo di 500 caratteri");
+        }
+        this.descrizione = descrizione;
+    }
+
+    public void setDifferenzaLike(int differenzaLike) {this.differenzaLike = differenzaLike;}
+
+    //getter
+    public int getVoto() {return voto;}
+    public String getDescrizione() {return descrizione;}
+    public int getDifferenzaLike() {return differenzaLike;}
+    public Fattura getFattura() {return fattura;}
 }
