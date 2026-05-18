ab2672715cb983ba1cc01aaa3b86e2a80310d0d2
Cristian Tardino <c.tardino@studenti.unina.it>
2026-05-18 03:37:11 +0200

    Creata classe e aggiunti getter e setter

--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -1,29 +1,58 @@
 package model;
 
-/**
- * The type Utente.
- */
-public class Utente {
-    private final String login;
-    private String password;
+import java.time.LocalDate;
 
-    /**
-     * Instantiates a new Utente.
-     *
-     * @param login    the login
-     * @param password the password
-     */
-    public Utente(String login, String password) {
-        this.login = login;
-        this.password = password;
+public class Utente extends Account {
+    private String genere; //valutare se mettere un char singolo/una lista di valori
+    private String email;
+    private int saldo;
+    private boolean bannato;
+    private LocalDate dataNascita;
+
+    public String getGenere() {
+        return genere;
     }
 
-    /**
-     * Gets login.
-     *
-     * @return the login
-     */
-    public String getLogin() {
-        return login;
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isBannato() {
+        return bannato;
+    }
+
+    public void setBannato(boolean bannato) {
+        this.bannato = bannato;
+    }
+
+    public LocalDate getDataNascita() {
+        return dataNascita;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Utente(String genere, String email, String nome, String password) {
+        super(nome, password);
+        setGenere(genere);
+        setEmail(email);
+        setSaldo(0);
+        setBannato(false);
+        //this.dataNascita = LocalDate.of(2000, 10, 10);
+        this.carrello = new Carrello(this);
     }
 }
