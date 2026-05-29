9dc774488dcfc0df436e89a94a5abf452bfd342b
Cristian Tardino <c.tardino@studenti.unina.it>
2026-05-29 20:02:17 +0200

    Finite le bozze dello scorso commit

--- src/main/java/gui/HomeAdmin.java
+++ src/main/java/gui/HomeAdmin.java
@@ -4,14 +4,13 @@ import controller.Controller;
 import model.Admin;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class HomeAdmin {
     private JPanel adminPanel;
     private JTabbedPane tabbedPane1;
     private JButton logoutButton;
-    private JList list1;
     private JTextField textField1;
-    private JButton cercaButton;
     private JList list2;
     private JList list3;
     private JList list4;
@@ -19,17 +18,14 @@ public class HomeAdmin {
     private JList list5;
     private JTextArea textArea1;
     private JCheckBox bannatiCheckBox;
-    private JButton rimuoviGenereButton;
     private JButton aggiungiGenereButton;
     private JTextField textField2;
     private JTextField textField3;
-    private JButton cercaButton1;
+    private JButton alfabeticoButton;
     private JButton bannaSbannaButton;
-    private JButton rimuoviRecensioneButton;
     private JList list6;
     private JButton rimborsaButton;
     private JTextField textField4;
-    private JButton cercaButton2;
     private JButton rimuoviPiattaformaButton;
     private JCheckBox checkBox1;
     private JTextField textField5;
@@ -37,7 +33,17 @@ public class HomeAdmin {
     private JTextField textField7;
     private JTextField textField8;
     private JTextField textField9;
-    private JButton aggiungiPromozioneATempoButton;
+    private JButton rimuoviRecensioneButton;
+    private JTable table1;
+    private JList list1;
+    private JButton aggiungiPromozioneButton;
+    private JTextField textField10;
+    private JButton ordinaPerDataButton;
+    private JTextField textField11;
+    private JCheckBox bannatoCheckBox;
+    private JButton bannaSbannaButton1;
+    private JList list7;
+    private JList list8;
 
     private JFrame adminFrame;
 
@@ -45,7 +51,9 @@ public class HomeAdmin {
         adminFrame = new JFrame("HomeAdmin");
         adminFrame.setContentPane(adminPanel);
         adminFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        adminFrame.setMinimumSize(new Dimension(900, 600));
         adminFrame.pack();
+        adminFrame.setLocationRelativeTo(null);
         adminFrame.setVisible(true);
     }
 }
