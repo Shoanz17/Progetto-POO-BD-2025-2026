316ce78721cb9bf8d1edd984a5db9c529bc71b2a
Antonio Orefice <antonio.orefice8@studenti.unina.it>
2026-07-25 22:00:55 +0200

    Tolto il main da accedi per far funzionare il Main.java correttamente come avviatore del programma.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,7 @@
-import model.Account;
-import model.Utente;
+import gui.Accedi;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); 
-        System.out.println("mertensss");
-
-
+        new Accedi();
     }
 }
